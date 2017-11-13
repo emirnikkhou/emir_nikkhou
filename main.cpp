ad96ff4aa0904cc17b65fe162877053c592a13b3
emir_nikkhou <emir_nikkhou@yahoo.com>
2017-11-13 11:39:52 +0330

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std ;
-
-int main()
+int factorial (int n)
 {
-	
-	int n ;
-	cin >> n ;
 	int ans = 1 ;
 	for (int i=0 ; i<=n ; i++)
-	{
-		ans = ans *  n ;
-	}
-	
-	cout << ans ;
+		ans = ans *  n ;	
+	return ans;
+}
+int main()
+{
+	int n ;
+	cin >> n ;
+	cout << factorial(n) ;
+	return 0 ;
 }
